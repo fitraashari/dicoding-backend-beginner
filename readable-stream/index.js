3b9efd70f2b3c0bd42538335bab80cb0a92eab8e
fitraashari <fitra.drive@gmail.com>
2021-04-14 16:36:00 +0700

    readable stream

---
+++ readable-stream/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const readableStream = fs.createReadStream('./articles.txt',{
+    highWaterMark: 10
+});
+
+readableStream.on('readable',()=>{
+    try {
+        process.stdout.write(`[${readableStream.read()}]`)
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+readableStream.on('end',()=>{
+    console.log('Done');
+});
