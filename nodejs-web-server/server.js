d911c4ccdf1320d22add9ee3e53df526ebe830c8
fitraashari <fitra.drive@gmail.com>
2021-04-15 16:07:00 +0700

    routing request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -4,32 +4,37 @@ const host = 'localhost';
 const requestListener = (request,response)=>{
     response.setHeader('Content-Type','text/html');
     response.statusCode = 200;
-    const {method} = request;
-    if (method==='GET') {
-        response.end('<h1>Ini GET</h1>')
-        
+    const {method,url} = request;
+
+    if (url==='/') {
+        if (method==='GET') {
+            response.end('Ini adalah home page');
+        }else{
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`);
+
+        }
+    }else if(url==='/about'){
+        if (method==='GET') {
+            response.end(`Hlo! Ini adalah halaman about`)
+        }else if (method==='POST') {
+            let body = [];
+            request.on('data',(chunk)=>{
+                body.push(chunk);
+            })
+            request.on('end',(chunk)=>{
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body)
+                response.end(`Halo, ${name} ini adalah halaman about`)
+            })
+        }else{
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`)
+
+        }
+    }else{
+        response.end(`Halaman tidak ditemukan!`)
+
     }
-    if (method==='POST') {
-        // response.end('<h1>Ini POST</h1>')
-        let body = [];
-        request.on('data',(chunk)=>{
-            body.push(chunk);
-        })
-        request.on('end',(chunk)=>{
-            body = Buffer.concat(body).toString();
-            const {name} = JSON.parse(body)
-            response.end(`<h1>Hai ${name}</h1>`);
-        })
-        
-    }
-    if (method==='PUT') {
-        response.end('<h1>Ini PUT</h1>')
-        
-    }
-    if (method==='DELETE') {
-        response.end('<h1>Ini DELETE</h1>')
-        
-    }
+
     
 }
 const server = http.createServer(requestListener);
